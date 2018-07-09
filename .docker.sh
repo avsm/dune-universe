9ccccd7d2a65109e5c972f96c1b41d7183669cd3
Anil Madhavapeddy <anil@recoil.org>
2018-07-09 21:20:26 +0100

    fix install-depext invocation
    Signed-off-by: Anil Madhavapeddy <anil@recoil.org>

--- .docker.sh
+++ .docker.sh
@@ -2,9 +2,9 @@
 DISTRO=${DISTRO:-alpine-3.7}
 
 set -ex
-make install-depext
 sudo chown -R opam /home/opam/src
 cd /home/opam/src
+make install-depext
 opam pin add -n jbuilder --dev
 opam pin add -y dune https://github.com/ocaml/dune.git
 make install-base
